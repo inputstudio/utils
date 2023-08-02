a811569d2c1e64606ac183d0d4bc164e08855e31
Kouassi Henri Joel <khenrijo21@gmail.com>
2023-08-02 22:59:43 +0000

    fix: wip - formatCurrency test fix

--- src/currency/formatCurrency.test.ts
+++ src/currency/formatCurrency.test.ts
@@ -4,24 +4,24 @@ import { test, expect } from 'vitest';
 test('formatCurrency function should format the given value to a currency', () => {
   const value = 1234.56;
   const currency = 'USD';
-  const expected = '$1,235';
+  const expected = '1 234,56 $US';
   const actual = formatCurrency(value, currency);
-  expect(actual).toBe(expected);
+  expect(actual).toEqual(expected);
 });
 
 test('formatCurrency function should format the given value to a currency using specified locale', () => {
   const value = 1234.56;
   const currency = 'EUR';
   const locale = 'de-DE';
-  const expected = '1.234 €';
+  const expected = '1.234,56 €';
   const actual = formatCurrency(value, currency, locale);
-  expect(actual).toBe(expected);
+  expect(actual).toEqual(expected);
 });
 
 test('formatCurrency function should format the given value to a currency with zero decimal places', () => {
   const value = 1234.56;
   const currency = 'JPY';
-  const expected = '¥1,235';
+  const expected = '1 235 JPY';
   const actual = formatCurrency(value, currency);
-  expect(actual).toBe(expected);
+  expect(actual).toEqual(expected);
 });
