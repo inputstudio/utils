f2e440a597273b9dcfb74cb13df8cd81c159886a
Alex N'Guessan <alexmarcaureln@gmail.com>
2023-08-02 17:29:30 +0000

    chore: associate test files to functions

---
+++ src/string/secret.test.ts
@@ -0,0 +1,42 @@
+import { secret } from './secret';
+import { test, expect } from 'vitest';
+
+test('should hide characters at the start of the string', () => {
+  const value = 'password123';
+  const expected = 'password***';
+  const actual = secret(value, '*', 8, 'start');
+
+  expect(actual).toBe(expected);
+});
+
+test('should hide characters at the end of the string', () => {
+  const value = 'password123';
+  const expected = '********123';
+  const actual = secret(value, '*', 3, 'end');
+
+  expect(actual).toBe(expected);
+});
+
+test('should throw an error if the value is empty', () => {
+  const value = '';
+  const expectedError = new Error('The string value should have a least one character');
+
+  expect(() => secret(value)).toThrow(expectedError);
+});
+
+test('should throw an error if the char length is greater than 1', () => {
+  const value = 'password123';
+  const char = '**';
+  const expectedError = new Error('The char should be only one character');
+  
+  expect(() => secret(value, char)).toThrow(expectedError);
+});
+
+test('should throw an error if the nbChars is greater than the value length', () => {
+  const value = 'password123';
+  const nbChars = 20;
+  const expectedError = new Error(
+    'The number of characters to extract from the former string exceed the string lenght'
+  );
+  expect(() => secret(value, '*', nbChars)).toThrow(expectedError);
+});
